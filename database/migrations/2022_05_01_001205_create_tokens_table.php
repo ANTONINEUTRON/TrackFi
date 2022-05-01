6be9bba19ddccaba560d9288467c2a613b34a84f
ANTONINEUTRON <antonineutron@gmail.com>
2022-05-01 01:35:03 +0100

    At this point, token migration was undertaken

---
+++ database/migrations/2022_05_01_001205_create_tokens_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTokensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tokens', function (Blueprint $table) {
+            $table->string("token_id",100)->primary();
+            $table->text("name");
+            $table->string("ticker",100);
+            $table->integer("decimals");
+            $table->double("price");
+            $table->double("price_change_24");
+            $table->double("token_value_in_dollars");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tokens');
+    }
+}
