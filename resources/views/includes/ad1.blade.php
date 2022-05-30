68c71fab5d4ffdd7b766fd5dba2ea7bf059af692
AntoniNeutron <antonineutron@gmail.com>
2022-05-31 00:54:45 +0100

    At this adverts was showing as specified-WAITING FOR DEPLOY ORDER

--- resources/views/includes/ad1.blade.php
+++ resources/views/includes/ad1.blade.php
@@ -1,16 +1,6 @@
 <!-- AD1 Mobile-->
 <div class="d-block d-md-none mt-1 crossed mr-2 ml-2"
 style="height: 10%; background-color: #00ffbb;margin-left: 1.5em;margin-right: 1.5em;">
-    {{-- @if (!empty($listOfAds))
-        <a target="_blank" href="{{$listOfAds['urls'][1]}}">
-        <img style="width: 100%; height:100%;" src="{{url('public/images/ads/'.$listOfAds["small_banner_url1"])}}" />
-        </a>
-    @else
-    <svg>
-        <line x1="0" y1="100%" x2="100%" y2="0" />
-        <line x1="0" y1="0" x2="100%" y2="100%" />
-    </svg>
-    @endif --}}
 
     @if (!empty($smallBanner1))
         <div class="crossed" style="height: 15%;">
